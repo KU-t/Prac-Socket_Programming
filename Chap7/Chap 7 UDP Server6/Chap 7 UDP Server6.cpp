393fbd2ab841a4fe36b13ad093aec391edb6601c
snrn2426@kpu.ac.kr <jhltk2426@naver.com>
2019-09-03 01:47:06 +0900

    [ch7] UDP Server - Client IPv6
    - #include<ws2tcpip.h>
    - socket(AF_INET,*,*) -> socket(AF_INET6,*,*)
    - SOCKADDR_IN socketaddr -> SOCKADDR_IN6 socketaddr
     -- INADDR_ANY -> in6addr_any
     -- inet_ntoa() -> WSAAddressToString()
     -- inet_addr() -> WSAStringToAddress()

---
+++ Chap7/Chap 7 UDP Server6/Chap 7 UDP Server6.cpp
@@ -0,0 +1,109 @@
+#pragma comment(lib, "ws2_32")
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SERVERPORT	9000
+#define BUFSIZE		512
+
+// 소켓 함수 오류 출력 후 종료 - [프로그램 실행]
+void err_quit(const char *msg) {
+	LPVOID lpMsgBuf;
+	// 오류메세지 얻기
+	FormatMessage(
+		// 오류메세지 저장 공간을 알아서 할당 | 운영체제로부터 오류 메세지 가져옴
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		// NULL , 오류코드 - [구체적 오류 코드 확인]
+		NULL, WSAGetLastError(),
+		//오류메세지 표시 언어 - 사용자 제어판 기본 설정 언어
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		// 오류 문자열 시작주소, nSize, Arguments
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	// 메세지 상자 생성 - [오류 메세지 확인]
+	MessageBox(NULL, (LPCTSTR)lpMsgBuf, (LPCTSTR)msg, MB_ICONERROR);
+	// 시스템 할당 메모리 반환
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+// 소켓 함수 오류 출력 - 테스트용
+void err_display(const char *msg) {
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	printf("[%s] %s", msg, (char *)lpMsgBuf);
+	// 시스템 할당 메모리 반환
+	LocalFree(lpMsgBuf);
+}
+
+int main(int argc, char *argv[]) {
+
+	int retval;
+
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
+	// socket() : 프로토콜 결정
+	SOCKET sock = socket(AF_INET6, SOCK_DGRAM, 0);
+	if (sock == INVALID_SOCKET)
+		err_quit("socket()");
+
+	// bind() : 지역 IP주소 & 지역 포트번호 결정
+	SOCKADDR_IN6 serveraddr;
+	ZeroMemory(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin6_family = AF_INET6;
+	serveraddr.sin6_addr = in6addr_any;
+	serveraddr.sin6_port = htons(SERVERPORT);
+	retval = bind(sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR)
+		err_quit("bind()");
+
+	// 데이터 통신에 사용할 변수
+	SOCKADDR_IN6 clientaddr;
+	int addrlen;
+	char buf[BUFSIZE + 1];
+
+	// 클라이언트와 데이터 통신
+	while (true) {
+		// 클라이언트가 보낸 데이터 받기
+		// recvfrom() : 클라이언트가 보낸 데이터 받기
+		// - 원격 IP주소 & 포트번호 확인
+		addrlen = sizeof(clientaddr);
+		retval = recvfrom(sock, buf, BUFSIZE, 0, (SOCKADDR*)&clientaddr, &addrlen);
+		if (retval == SOCKET_ERROR) {
+			err_display("recvfrom()");
+			continue;
+		}
+
+		// 주소 변환 [ IPv6 -> 문자열 ]
+		char ipaddr[50];
+		DWORD ipaddrlen = sizeof(ipaddr);
+		WSAAddressToString((SOCKADDR*)&clientaddr, sizeof(clientaddr), NULL, ipaddr, &ipaddrlen);
+
+		// 받은 데이터 출력
+		buf[retval] = '\0';
+		printf("[UDP / %s ] %s \n", ipaddr, buf);
+
+		// 클라이언트에 데이터 보내기
+		// sendto() : 받은 데이터를 처리해 클라이언트에 보내기
+		retval = sendto(sock, buf, retval, 0, (SOCKADDR*)&clientaddr, sizeof(clientaddr));
+		if (retval == SOCKET_ERROR) {
+			err_display("sendto()");
+			continue;
+		}
+	}
+
+	// closesocket()
+	closesocket(sock);
+
+	// 윈속 종료
+	WSACleanup();
+	return 0;
+}
